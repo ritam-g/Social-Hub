33c5d3698cb49b1154b1ed7a64691d80fed0107e
Ritam Maty <ritammaty@gmail.com>
2025-03-10 18:00:31 +0530

    set up app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const cookie_parser=require('cookie-parser');
+const path=require('path');
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookie_parser());
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine','ejs');
+app.get('/',(req,res)=>{
+    res.send("helcome to home page");
+});
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+});
